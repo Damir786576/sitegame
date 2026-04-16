76a85e7e3c2da7b1d8c9d32128e6d7aecc9e56b9
Student <andreevdamir266@gmail.com>
2026-04-16 15:09:12 +0300

    дз2

---
+++ hw2.js
@@ -0,0 +1,55 @@
+// 1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+//2
+const iphone = 2007;
+alert(iphone);
+
+//3
+const nameCreator = "Брендан";
+alert(nameCreator);
+
+//4
+let b = 10;
+let c = 2;
+alert(b-c);
+alert(b+c);
+alert(b/c);
+
+//5
+let d = 2;
+result = 2**5;
+alert(result);
+
+//6
+let num1 = 9;
+let num2 = 2;
+alert(9%2);
+
+//7 
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+//8 
+let age = prompt('Сколько вам лет?');
+alert(age);
+
+//9
+let User = {
+    name:'Дамир',
+    age:18,
+    isAdmin: true
+}
+
+//10
+let name2 = prompt("Как тебя зовут?")
+alert(`Привет, ${name2}`)
